3168d374426d4883add80a8bc47e8bd9d6acc26d
Koki Takahashi <hakatasiloving@gmail.com>
2022-03-23 06:52:02 +0900

    Implement dynamix note

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
-import {Canvas, useFrame} from '@react-three/fiber';
-import React, {useRef, useState, useCallback} from 'react';
+import {Canvas, useFrame, useLoader} from '@react-three/fiber';
+import React, {useRef, useState, useCallback, Suspense} from 'react';
 import ReactDOM from 'react-dom';
+import {AdditiveBlending, TextureLoader, Vector2} from 'three';
+import dynamixBackgroundImg from './images/dynamix-background.png';
+import dynamixNoteImg from './images/dynamix-note.png';
+import dynamixNoteImg2 from './images/dynamix-note2.png';
+import dynamixNoteImg3 from './images/dynamix-note3.png';
 
 const Box = (props) => {
 	const ref = useRef();
@@ -8,11 +13,14 @@ const Box = (props) => {
 	const [hover, setHover] = useState(false);
 	const [click, setClick] = useState(false);
 
-	useFrame(() => (ref.current.rotation.x += 0.01));
+	useFrame(() => {
+		ref.current.rotation.x += 0.01;
+		ref.current.rotation.y += 0.01;
+	});
 
 	const handlePointerOver = useCallback(() => setHover(true), [setHover]);
 	const handlePointerOut = useCallback(() => setHover(false), [setHover]);
-	const handleClick = useCallback(() => setClick(true), [setClick]);
+	const handleClick = useCallback(() => setClick(!click), [setClick, click]);
 
 	return (
 		<mesh
@@ -23,18 +31,96 @@ const Box = (props) => {
 			onPointerOver={handlePointerOver}
 			onPointerOut={handlePointerOut}
 		>
-			<boxGeometry args={[1, 1, 1]}/>
-			<meshStandardMaterial color={hover ? 'hotpink' : 'orange'}/>
+			<boxGeometry args={[100, 100, 100]}/>
+			<meshLambertMaterial color={hover ? 'hotpink' : 'orange'}/>
 		</mesh>
 	);
 };
 
+const DynamixBackground = () => {
+	const texture = useLoader(TextureLoader, dynamixBackgroundImg);
+
+	return (
+		<sprite
+			position={[0, 0, -200]}
+			scale={[texture.image.width, texture.image.height, 1]}
+		>
+			<spriteMaterial attach="material" map={texture} sizeAttenuation={false}/>
+		</sprite>
+	);
+};
+
+const NoteLeft = (props: {x: number, y: number, width: number}) => {
+	const texture = useLoader(TextureLoader, dynamixNoteImg2);
+	texture.offset = new Vector2(0, 0);
+	texture.repeat.set(0.45, 1);
+
+	return (
+		<sprite
+			position={[props.x - props.width / 2 - texture.image.width * 0.45 / 2, props.y, -200]}
+			scale={[texture.image.width * 0.45, texture.image.height, 1]}
+		>
+			<spriteMaterial attach="material" map={texture} sizeAttenuation={false} blending={AdditiveBlending}/>
+		</sprite>
+	);
+};
+
+const NoteCenter = (props: {x: number, y: number, width: number}) => {
+	const texture = useLoader(TextureLoader, dynamixNoteImg);
+	texture.offset = new Vector2(0.45, 0);
+	texture.repeat.set(0.1, 1);
+
+	return (
+		<sprite position={[props.x, props.y, -200]} scale={[props.width, texture.image.height, 1]}>
+			<spriteMaterial attach="material" map={texture} sizeAttenuation={false} blending={AdditiveBlending}/>
+		</sprite>
+	);
+};
+
+const NoteRight = (props: {x: number, y: number, width: number}) => {
+	const texture = useLoader(TextureLoader, dynamixNoteImg3);
+	texture.offset = new Vector2(0.55, 0);
+	texture.repeat.set(0.45, 1);
+
+	return (
+		<sprite
+			position={[props.x + props.width / 2 + texture.image.width * 0.45 / 2, props.y, -200]}
+			scale={[texture.image.width * 0.45, texture.image.height, 1]}
+		>
+			<spriteMaterial attach="material" map={texture} sizeAttenuation={false} blending={AdditiveBlending}/>
+		</sprite>
+	);
+};
+
+const Note = (props: {x: number, y: number, width: number}) => {
+	const [timer, setTimer] = useState(0);
+
+	useFrame(() => {
+		setTimer(timer + 1);
+	});
+
+	const offset = -(timer % 100) * 10;
+
+	return (
+		<group>
+			<NoteLeft x={props.x} y={props.y + offset} width={props.width}/>
+			<NoteCenter x={props.x} y={props.y + offset} width={props.width}/>
+			<NoteRight x={props.x} y={props.y + offset} width={props.width}/>
+		</group>
+	);
+};
+
 ReactDOM.render(
-	<Canvas>
+	<Canvas orthographic>
+		<color attach="background" args={[0, 0, 0]}/>
 		<ambientLight/>
-		<pointLight position={[10, 10, 10]}/>
-		<Box position={[-1.2, 0, 0]}/>
-		<Box position={[1.2, 0, 0]}/>
+		<pointLight position={[0, 0, 0]}/>
+		<Box position={[0, -300, -100]}/>
+		<Suspense fallback={<>Loading...</>}>
+			<DynamixBackground/>
+			<Note x={0} y={0} width={300}/>
+			<Note x={200} y={250} width={150}/>
+		</Suspense>
 	</Canvas>,
 	document.getElementById('root'),
 );
--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
+/*
+import {Scene, OrthographicCamera, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshLambertMaterial, Mesh, PointLight, MeshBasicMaterial} from 'three';
+
+const scene = new Scene();
+const camera = new OrthographicCamera(-100, 100, -100, 100, 0.1, 1000);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({color: 0x00ff00});
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+const light = new PointLight(0xffffff, 1, 100);
+light.position.set(0, 0, 3);
+scene.add(light);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+animate();
+*/
+
 import './App';
