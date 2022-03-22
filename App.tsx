50de6505ef5e4667d9cf9985ad13f01a19bcf7a1
Koki Takahashi <hakatasiloving@gmail.com>
2022-03-23 03:22:31 +0900

    Setup ESLint

--- App.tsx
+++ App.tsx
@@ -1,35 +1,40 @@
-import ReactDOM from 'react-dom'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import {Canvas, useFrame} from '@react-three/fiber';
+import React, {useRef, useState, useCallback} from 'react';
+import ReactDOM from 'react-dom';
 
 const Box = (props) => {
-  const ref = useRef()
+	const ref = useRef();
 
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+	const [hover, setHover] = useState(false);
+	const [click, setClick] = useState(false);
 
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+	useFrame(() => (ref.current.rotation.x += 0.01));
 
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+	const handlePointerOver = useCallback(() => setHover(true), [setHover]);
+	const handlePointerOut = useCallback(() => setHover(false), [setHover]);
+	const handleClick = useCallback(() => setClick(true), [setClick]);
+
+	return (
+		<mesh
+			{...props}
+			ref={ref}
+			scale={click ? 1.5 : 1}
+			onClick={handleClick}
+			onPointerOver={handlePointerOver}
+			onPointerOut={handlePointerOut}
+		>
+			<boxGeometry args={[1, 1, 1]}/>
+			<meshStandardMaterial color={hover ? 'hotpink' : 'orange'}/>
+		</mesh>
+	);
+};
 
 ReactDOM.render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
-  document.getElementById('root'),
-)
+	<Canvas>
+		<ambientLight/>
+		<pointLight position={[10, 10, 10]}/>
+		<Box position={[-1.2, 0, 0]}/>
+		<Box position={[1.2, 0, 0]}/>
+	</Canvas>,
+	document.getElementById('root'),
+);
